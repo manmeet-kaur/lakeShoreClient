37b3376eddec2648de147c3eb7f77e7dca5a7cf3
manmeetkaur <mrangoola@luc.edu>
2015-11-22 15:33:44 -0600

    merged syeda's changes

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -5,45 +5,98 @@
 		<!-- Use Bootstrap -->
 		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular.min.js"></script>
+		<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular-route.js"></script>
 		<script type="text/javascript" src="static/js/app.js"></script>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 	
-	<body ng-controller="HttpCtrl as app">
-  	<div class="container">
-		<div class="header">
-			<h1 class="custom">Welcome to Lake Shore Mart</h1>
-		</div>
-		<div class="container">
-		    <form>
-		    	<table>
-					<tr>
-						<td><input type="text" ng-model="searchName" size="30"></td>
-						<td><button type="button" ng-click="searchProduct(searchName)" class="btn btn-primary btn-sm">
-      					<span class="glyphicon glyphicon-search"></span> Search </button></td>
-<!-- 						<td><button ng-click="addNew()" class="btn btn-primary btn-sm">
-						<span class="glyphicon glyphicon-plus"></span> Add New </button></td>
- -->						<td><button ng-click="resetSearch()"  class="btn btn-info btn-sm">
-						<span class="glyphicon glyphicon-refresh"></span> Reset Search </button></td>
-					</tr>
-				</table>
-		    </form>					
-		</div>
-		<div ng-repeat="product in products" ng-show=app.isHome>
-			<h3>{{product.productName}}</h3>
-			<p>Price: {{product.price}}</p>
-			<p>Quantity: {{product.quantity}}</p>
-			<button ng-click="addToCart(product.productId)">Add to Cart</button>
-		</div>
+	<body ng-controller="mainController as main">
+		<nav class="navbar navbar-inverse">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="#">Lake Shore Mart</a>
+				</div>
+				<div>
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="#">Home</a></li>
+						<li><a href="#about">About</a></li>
+						<li><a href="#contact">Contact</a></li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="register.html"><span class="glyphicon glyphicon-user"></span> Login </a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	
+	  	<div class="container">
+			<div class="header">
+				<h1 class="custom">Welcome to Lake Shore Mart</h1>
+			</div>
+			
+			<div class="container">
+				<p></p>
+			    <form>
+			    	<table>
+						<tr>
+							<td><label for="search">Enter Search String here: </label>
+							<td><input type="text" ng-model="searchName" size="30"></td>
+							<td><button type="button" ng-click="searchProduct(searchName)" class="btn btn-primary btn-sm">
+	      					<span class="glyphicon glyphicon-search"></span> Search </button></td>
+							<td><button ng-click="resetSearch()"  class="btn btn-info btn-sm">
+							<span class="glyphicon glyphicon-refresh"></span> Reset Search </button></td>
+						</tr>
+					</table>
+			    </form>				
+				<ul class="list-group">
+					<li class="list-group-item" ng-repeat="product in products" ng-hide="main.cartSuccess">
+						<h4>{{product.productName}}</h4>
+						<em class="pull-right">{{product.price | currency}}</em>
+						<p>Quantity: {{product.quantity}}</p>
+						<a href="#" ng-click="addToCart(product.productId, product.links[0].url)">Add to Cart</a>
+					</li>
+				</ul>
+				
+				<!-- Division for showing cart status message -->
+				
+				<div class="container" ng-show="main.cartSuccess">
+					<h2>{{info.message}}</h2>
+					<a href="#" ng-click="viewCart(info.links[0].url)">View Cart</a>
+					<a href="#" ng-click="placeOrder(info.links[1].url)">Place Order</a>
+				</div>
+				
+				<!-- Division for showing cart contents -->
+				
+				<div class=container  ng-show="main.showCart">
+				<ul class="list-group">
+					<li class="list-group-item" ng-repeat="item in items">
+						<h4>{{item.productId}}</h4>
+						<em class="pull-right">Quantity: {{item.quantity}}</em>
+					</li>
+				</ul>
+				<a href="#" ng-click="placeOrder(info.links[1].url)">Place Order</a>
+				</div>
+				
+				<!-- Division for showing order Status Message -->
 
-		<div ng-hide=isHome ng-show=app.isCartSuccess>
-			<h3>{{info.message}}</h3>
-			<button ng-click="placeOrder()">Place Order</button>
+				<div class=container ng-show="main.orderSuccess">
+					<h2>{{info.message}}</h2>
+					<a href="#" ng-click="viewCart(info.links[0].url)">View Cart</a>
+					<a href="#" ng-click="placeOrder(info.links[1].url)">Place Order</a>
+				</div>				
+			</div>
+			<p></p>
+			<div class="footer">@Copyright WebServicesGroup</div>
 		</div>
-
-		<div class="footer">@Copyright WebServicesGroup</div>
-	</div>
+		
+				<div>
+			<br>
+			<button ng-click="viewAllCustomer()">View All Customer Details</button>
+		</div>
 	</body>
-	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	
+	<!-- Scripts for bootstrap and jquery -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
 </html>
--- WebContent/static/js/app.js
+++ WebContent/static/js/app.js
@@ -1,75 +1,72 @@
 (function() {
 	
-	var app = angular.module("app", []);
-
+	var app = angular.module("app", ['ngRoute']);
 	
-	app.controller("HttpCtrl", function($scope, $http) {
+	app.config(['$routeProvider',
+	            function($routeProvider) {
+	              $routeProvider.
+	                when('/', {
+	                  templateUrl: 'index.jsp',
+	                  controller: 'mainController'
+	              }).
+	                when('/cartAdded', {
+	                  templateUrl: 'views/status.html',
+	                }).
+	                otherwise({
+	                  redirectTo: '/index.jsp'
+	                });
+	          }]);
+	
+	app.controller("mainController", function($scope, $http, $window) {
 		var baseUrl = 'http://localhost:8080';
 		var app = this;
 		$scope.navTitle = 'All Products';
-		$scope.home = true;
-		$scope.cartSuccess = false;
-		$scope.operation="";
-		$scope.isSaveDisabled = true;
-		$scope.isDeleteDisabled = true;
+		app.home = true;
+		app.cartSuccess = false;
+		this.showCart = false;
+		this.orderSuccess = false;
 		
-		this.isHome = function() {
-			return $scope.home;
-		}
-		
-		var response = $http.get('http://localhost:8080/products');
+		// retrieve all products
+		var response = $http.get(baseUrl + '/products');
 		response.success(function(data) {
 			$scope.products = data;
+			console.log("isHome " + app.home)
 			console.log("[main] # of items: " + data.length)
 			angular.forEach(data, function(element) {
-				console.log("[main] product: " + element.name);
+				console.log("[main] product: " + element.productName);
 			});
 		})
 		response.error(function(data, status, headers, config) {
 			alert("Failed to get data from " + baseUrl + "/products- status =" + status);
 		})
 		
-		$scope.addToCart = function(productId) {
+
+		// add a product to the cart
+		$scope.addToCart = function(productId, url) {
 			var cartRequest = {
 				customerId: 10000174,
 				productId: productId,
 				quantity: 1
 			};
-			var response = $http.post(baseUrl + '/cart/add', cartRequest);
+			var response = $http.post(url, cartRequest);
 			response.success(function(data){
 				$scope.info = data;
-				$scope.cartSuccess = true;
-				$scope.home = false;
+				app.cartSuccess = true;
 			})
 			response.error(function(data, status, headers, config) {
 				alert("Failed to add to cart, status = " + status);
 			})
 		};
 		
-		$scope.isCartSuccess = function() {
-			return $scope.cartSuccess;
-		}
 		
-//		$scope.getProduct = function(name) {
-//			var response = $http.get('/product/?name='+ name );
-//			
-//			response.success(function(data) {
-//				$scope.product = data;
-//		    })
-//			
-//			response.error(function(data, status, headers, config) {
-//				alert("Failed to get data, status=" + status);
-//			})
-//		};
-		
+		// search a product by product name
 		$scope.searchProduct = function(name) {
 			var app = this;
 			$scope.navTitle = 'Search Criteria';
 			
-			var response = $http.get('http://localhost:8080/product/?name=' + name);
+			var response = $http.get(baseUrl + '/product/?name=' + name);
 			response.success(function(data) {
 				$scope.products = data;
-
 				console.log("[searchProduct] # of items: " + data.length)
 				angular.forEach(data, function(element) {
 					console.log("[searchProduct] product: " + element.name);
@@ -82,80 +79,39 @@
 			})
 		};
 		
-		$scope.clearForm = function() {
-			$scope.actor = {
-					productId:'',
-					productName:'',
-					productType:'',
-					quantity:'',
-					vendorName:''
-			};
-		}
-		
-//		$scope.addNew = function(element) {
-//			$scope.operation="create";
-//			$scope.clearForm();
-//			main.id.focus();
-//			$scope.isSaveDisabled = false;
-//			$scope.isDeleteDisabled = true;
-//		}
-//		
-//		$scope.saveActor = function(id) {
-//			$scope.jsonObj = angular.toJson($scope.actor, false);
-//			console.log("[update] data: " + $scope.jsonObj);
-//
-//			if ($scope.operation == "update") {
-//				var response = $http.put('/RestfulWebServiceExample/rest/actors/' + id, $scope.jsonObj);
-//				response.success(function(data, status, headers, config) {
-//					$scope.resetSearch();
-//			    });
-//				
-//				response.error(function(data, status, headers, config) {
-//					alert("AJAX failed to get data, status=" + status);
-//				})
-//			} else if ($scope.operation == "create") {
-//				var response = $http.post('/RestfulWebServiceExample/rest/actors/add', $scope.jsonObj);
-//				response.success(function(data, status, headers, config) {
-//					$scope.resetSearch();
-//			    });
-//				
-//				response.error(function(data, status, headers, config) {
-//					alert("AJAX failed to get data, status=" + status);
-//				})	
-//			}
-//		};
-//		
-//		$scope.deleteActor = function(id) {
-//			var response = $http.delete('/RestfulWebServiceExample/rest/actors/' + id);
-//			response.success(function(data, status, headers, config) {
-//				$scope.resetSearch();
-//			});
-//				
-//			response.error(function(data, status, headers, config) {
-//				alert("AJAX failed to get data, status=" + status);
-//			})
-//		};
-		
-		$scope.resetSearch = function(name) {
-			var app = this;
-			$scope.operation="";
-			$scope.clearForm();
-			$scope.isSaveDisabled = true;
-			$scope.isDeleteDisabled = true;
-			$scope.navTitle = 'All Products';
-			$scope.searchName = '';
-			
-			var response = $http.get('/product');
-			response.success(function(data) {
-				$scope.actors = data;
-				$scope.$apply();
-				console.log("[resetSearch] # of items: " + data.length)
-		    });
-			
+		// view cart
+		$scope.viewCart = function(url) {
+			var response = $http.get(url);
+			response.succcess(function(data) {
+				this.items = data;
+				console.log("[viewCart] # of items: " + data.length);
+				angular.forEach(data, function(element) {
+					console.log("[viewCart] product:" + element.name + "-" + element.quantity);
+				});
+				this.showCart = true;
+				this.cartSuccess = false;
+			});
 			response.error(function(data, status, headers, config) {
 				alert("Failed to get data, status=" + status);
-			})
+			});
 		};
 		
+		// place order
+		$scope.placeOrder = function(url) {
+			var response = $http.get(url);
+			response.succcess(function(data) {
+				this.items = data;
+				console.log("[placeOrder] # of items: " + data.length);
+				this.orderSuccess = true;
+			});
+			response.error(function(data, status, headers, config) {
+				alert("Failed to get data, status=" + status);
+			});
+		};
+		
+		$scope.viewAllCustomer = function() {
+			var url = "http://" + $window.location.host + "/lakeShoreWebProject/customer.html";
+	        $window.location.href = url;
+		};
 	});	
 })();
